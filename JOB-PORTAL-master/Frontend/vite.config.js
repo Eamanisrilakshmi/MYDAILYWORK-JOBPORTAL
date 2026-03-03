a60320900a71f115a7adc51b71339936542416af
Eamanisrilakshmi <eamanisrilakshmi06@gmail.com>
2026-03-03 14:51:46 +0530

    Update vite.config.js

--- JOB-PORTAL-master/Frontend/vite.config.js
+++ JOB-PORTAL-master/Frontend/vite.config.js
@@ -1,8 +1,23 @@
+// import path from "path"
+// import react from "@vitejs/plugin-react"
+// import { defineConfig } from "vite"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/MYDAILYWORK-JOBPORTAL/",   // 👈 ADD THIS LINE
   plugins: [react()],
   resolve: {
     alias: {
